a83b34a93ee728e44f06d74f4eb4954af880fc6e
ERICK MEDEIROS BRITO DA SILVA <a95607457@rn.senac.local>
2025-05-06 21:28:09 -0300

    Criaçao de CRUD de Veiculos e ordem de Servico

--- src/modules/ordens_serviço/model/index.js
+++ src/modules/ordens_serviço/model/index.js
@@ -9,4 +9,35 @@ class OrdemServicoModel{
         return resultado.rows;
     }
 
+
+    static async listar_ordem_servico_veiculo(id_veiculo) { 
+        const dados = [id_veiculo];
+        const consulta = `
+        select ordens_servico.id_ordem_servico, ordens_servico.data_abertura, ordens_servico.status, clientes.nome, veiculos.modelo from ordens_servico 
+        join veiculos on ordens_servico.id_veiculo = veiculos.id_veiculo 
+        join clientes on veiculos.id_cliente = clientes.id_cliente 
+        where ordens_servico.id_veiculo = $1`;
+        const resultado = await client.query(consulta, dados);
+        return resultado.rows;
+    }
+
+    static async filtrar_ordem_servico_por_status(status) {
+        const dados = [status];
+        const consulta = `select ordens_servico.id_ordem_servico, ordens_servico.status, veiculos.modelo from ordens_servico 
+        join veiculos on ordens_servico.id_veiculo = veiculos.id_veiculo 
+        where ordens_servico.status = $1`;
+        const resultado = await client.query(consulta, dados);
+        return resultado.rows;
+    }
+
+    static async contar_somar_valor_por_cliente(id_cliente) {
+        const dados = [id_cliente];
+        const consulta = `
+        select count (ordens_servico.id_ordem_servico) as total_ordens_servico,sum (ordens_servico.valor) as total_valor, clientes.nome from ordens_servico
+        join veiculos on ordens_servico.id_veiculo = veiculos.id_veiculo
+        join clientes on veiculos.id_cliente = clientes.id_cliente 
+        where ordens_servico.id_cliente = $1`;
+        const resultado = await client.query(consulta, dados);
+        return resultado.rows;
+    }
 }
